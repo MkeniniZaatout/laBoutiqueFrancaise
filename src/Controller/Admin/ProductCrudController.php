b4268d674d24a91efdf09e8a9834d3414fa2adc8
MkeniniZaatout <mkeninismael2@gmail.com>
2021-04-29 02:38:51 +0200

    add entity product in admin page

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,7 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use App\Entity\Category;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -12,14 +20,24 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            SlugField::new('slug')->setTargetFieldName('name'),
+            ImageField::new('illustration')
+                ->setBasePath('uploads/')
+                ->setUploadDir('public/uploads')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(false),
+            TextField::new('subtitle'),
+            TextareaField::new('description'),
+            MoneyField::new('price')->setCurrency('EUR')
+            /*
+            ,
+            AssociationField::new('category')*/
         ];
     }
-    */
+    
 }
